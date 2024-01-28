ccecad9228ad8f48a595528a43d54cd0fc3fc9e9
lamanhdai <lamanhdai@users.noreply.github.com>
2024-01-28 10:38:30 +0000

    vue options api vs composition api

---
+++ src/AppOptions.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            message: ''
+        }
+    },
+    methods: {
+        greetMessage() {
+            this.message = 'Hello Vue Options'
+        }
+    },
+    created() {
+        this.greetMessage()
+    }
+}
+</script>
+
+<template>
+    <div>{{ message }}</div>
+</template>
+
+<style scoped>
+</style>
